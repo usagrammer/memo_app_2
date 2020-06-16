0c87e45573b7db3c549601e8cff7d7a24762d549
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-06-17 01:20:10 +0900

    commit

--- lib/views/posts/new.dart
+++ lib/views/posts/new.dart
@@ -3,6 +3,8 @@ import 'package:flutter_icons/flutter_icons.dart';
 import 'package:flutter_state_notifier/flutter_state_notifier.dart';
 import 'package:provider/provider.dart';
 
+import 'package:keyboard_actions/keyboard_actions.dart';
+
 import 'package:memoapp2/Controllers/footer_state.dart';
 
 class PostsNew extends StatelessWidget {
@@ -17,6 +19,36 @@ class PostsNew extends StatelessWidget {
     int category_id = 0;
     List categories = ["うさぎ", "プログラミング"];
     List postOfTags = ["タグ一覧", "ほげ"];
+
+    FocusNode _ContentSubmit = FocusNode();
+
+    KeyboardActionsConfig _buildConfig(BuildContext context) {
+      return KeyboardActionsConfig(
+        keyboardActionsPlatform: KeyboardActionsPlatform.ALL,
+        keyboardBarColor: Colors.grey[200],
+        nextFocus: true,
+        actions: [
+          KeyboardAction(focusNode: _ContentSubmit, toolbarButtons: [
+            //button 1
+            (node) {
+              return GestureDetector(
+                onTap: () => node.unfocus(),
+                child: Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.all(8.0),
+                  child: IconButton(
+                      icon: Icon(Icons.add_circle),
+                      onPressed: () {
+                        debugPrint('222');
+                        FocusScope.of(context).unfocus();
+                      }),
+                ),
+              );
+            },
+          ]),
+        ],
+      );
+    }
 
     return Scaffold(
       resizeToAvoidBottomPadding: false,
@@ -36,129 +68,136 @@ class PostsNew extends StatelessWidget {
             )
           ],
           title: Text('新規投稿')),
-      body: Column(
-        children: <Widget>[
-          TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                contentPadding: EdgeInsets.all(10),
-                hintText: 'タイトル',
-              ),
-              enabled: true,
-              // 入力数
-              maxLength: 10,
-              maxLengthEnforced: false,
-              style: TextStyle(color: Colors.red),
-              obscureText: false,
-              maxLines: 1,
-              //パスワード
-              onChanged: (text) {
-                title = text;
-                print("title_changed:${title}");
-              }),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: DropdownButton<int>(
-                  value: category_id,
-                  onChanged: (int) {
-                    category_id = int;
-                    print("changed_category_id:${category_id}");
-                  },
-                  items: categories.map((category) {
-                    print("build_Cateogyr");
-                    print(category);
-                    return DropdownMenuItem<int>(
-                      value: categories.indexOf(category),
-                      child: Text(category),
-                    );
-                  }).toList(),
+      body: KeyboardActions(
+        config: _buildConfig(context),
+        child: Column(
+          children: <Widget>[
+            TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  contentPadding: EdgeInsets.all(10),
+                  hintText: 'タイトル',
                 ),
-              ),
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    contentPadding: EdgeInsets.all(10),
-                    hintText: 'カテゴリを追加',
-                    suffixIcon: IconButton(
-                        icon: Icon(Icons.add_circle),
-                        onPressed: () {
-                          debugPrint('222');
-                        }),
+                enabled: true,
+                // 入力数
+                maxLength: 10,
+                maxLengthEnforced: false,
+                style: TextStyle(color: Colors.red),
+                obscureText: false,
+                maxLines: 1,
+                //パスワード
+                onChanged: (text) {
+                  title = text;
+                  print("title_changed:${title}");
+                }),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: DropdownButton<int>(
+                    value: category_id,
+                    onChanged: (int) {
+                      category_id = int;
+                      print("changed_category_id:${category_id}");
+                    },
+                    items: categories.map((category) {
+                      print("build_Cateogyr");
+                      print(category);
+                      return DropdownMenuItem<int>(
+                        value: categories.indexOf(category),
+                        child: Text(category),
+                      );
+                    }).toList(),
                   ),
-                  enabled: true,
-                  // 入力数
-                  maxLength: 10,
-                  maxLengthEnforced: false,
-                  style: TextStyle(color: Colors.red),
-                  obscureText: false,
-                  maxLines: 1,
                 ),
-              ),
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: DropdownButton<int>(
-                  value: category_id,
-                  onChanged: (int) {
-                    category_id = int;
-                    print("changed_category_id:${category_id}");
-                  },
-                  items: postOfTags.map((tag) {
-                    print("build_postOftags");
-                    print(tag);
-                    return DropdownMenuItem<int>(
-                      value: postOfTags.indexOf(tag),
-                      child: Text(tag),
-                    );
-                  }).toList(),
-                ),
-              ),
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    contentPadding: EdgeInsets.all(10),
-                    hintText: 'タグを追加',
-                    suffixIcon: IconButton(
-                        icon: Icon(Icons.add_circle),
-                        onPressed: () {
-                          debugPrint('222');
-                        }),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      contentPadding: EdgeInsets.all(10),
+                      hintText: 'カテゴリを追加',
+                      suffixIcon: IconButton(
+                          icon: Icon(Icons.add_circle),
+                          onPressed: () {
+                            debugPrint('222');
+                          }),
+                    ),
+                    enabled: true,
+                    // 入力数
+                    maxLength: 10,
+                    maxLengthEnforced: false,
+                    style: TextStyle(color: Colors.red),
+                    obscureText: false,
+                    maxLines: 1,
                   ),
-                  enabled: true,
-                  // 入力数
-                  maxLength: 10,
-                  maxLengthEnforced: false,
-                  style: TextStyle(color: Colors.red),
-                  obscureText: false,
-                  maxLines: 1,
                 ),
-              ),
-            ],
-          ),
-          TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                contentPadding: EdgeInsets.all(10),
-                hintText: '本文',
-              ),
-              enabled: true,
-              // 入力数
-              maxLength: 10,
-              maxLengthEnforced: false,
-              style: TextStyle(color: Colors.red),
-              obscureText: false,
-              maxLines: 10,
-              //パスワード
-              onChanged: (text) {
-                content = text;
-                print("content_changed:${content}");
-              }),
-        ],
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: DropdownButton<int>(
+                    value: category_id,
+                    onChanged: (int) {
+                      category_id = int;
+                      print("changed_category_id:${category_id}");
+                    },
+                    items: postOfTags.map((tag) {
+                      print("build_postOftags");
+                      print(tag);
+                      return DropdownMenuItem<int>(
+                        value: postOfTags.indexOf(tag),
+                        child: Text(tag),
+                      );
+                    }).toList(),
+                  ),
+                ),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      contentPadding: EdgeInsets.all(10),
+                      hintText: 'タグを追加',
+                      suffixIcon: IconButton(
+                          icon: Icon(Icons.add_circle),
+                          onPressed: () {
+                            debugPrint('222');
+                          }),
+                    ),
+                    enabled: true,
+                    // 入力数
+                    maxLength: 10,
+                    maxLengthEnforced: false,
+                    style: TextStyle(color: Colors.red),
+                    obscureText: false,
+                    maxLines: 1,
+                    onSubmitted: (text) {
+                      print("saved: ${text}");
+                    },
+                  ),
+                ),
+              ],
+            ),
+            TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  contentPadding: EdgeInsets.all(10),
+                  hintText: '本文',
+                ),
+                enabled: true,
+                focusNode: _ContentSubmit,
+                // 入力数
+                maxLength: 10,
+                maxLengthEnforced: false,
+                style: TextStyle(color: Colors.red),
+                obscureText: false,
+                maxLines: 10,
+                //パスワード
+                onChanged: (text) {
+                  content = text;
+                  print("content_changed:${content}");
+                }),
+          ],
+        ),
       ),
     );
   }
